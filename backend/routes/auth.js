ebd220d2d703988e91424bad649e66639586445b
Rudragiri_gosai <dipakgosai199@gmail.com>
2025-02-05 23:32:15 +0530

    Review api is complete.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { login, register } from "../controllers/authController.js"
+import { login, register } from "../controllers/authControllers.js"
 const router = express.Router()
 //Auth Registration
 router.post('/register', register)
